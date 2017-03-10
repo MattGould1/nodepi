ccc16675bdcd2c00128f35cb40a2d5564717cb42
Matt <matthew.gould@dentaldepartures.com>
2017-03-10 06:01:13 +0000

    add pwm back

--- src/server.js
+++ src/server.js
@@ -3,23 +3,44 @@ import express from 'express'
 import BodyParser from 'body-parser'
 //routes
 //import index from './routes/index'
-import wpi from 'wiring-pi'
 
-// GPIO pin of the led
-var configPin = 7;
-// Blinking interval in usec
-var configTimeout = 1000;
+import rpio from 'rpio'
 
-wpi.setup('wpi');
-wpi.pinMode(configPin, wpi.OUTPUT);
+var pin = 12;           /* P12/GPIO18 */
+var range = 1024;       /* LEDs can quickly hit max brightness, so only use */
+var max = 128;          /*   the bottom 8th of a larger scale */
+var clockdiv = 8;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
+var interval = 500;       /* setInterval timer, speed of pulses */
+var times = 5;          /* How many times to pulse before exiting */
 
-var isLedOn = 0;
+/*
+ * Enable PWM on the chosen pin and set the clock and range.
+ */
+rpio.open(pin, rpio.PWM);
+rpio.pwmSetClockDivider(clockdiv);
+rpio.pwmSetRange(pin, range);
 
-setInterval(function() {
-	isLedOn = +!isLedOn;
-	//isLedOn = !isLedOn;
-	wpi.digitalWrite(configPin, isLedOn );
-}, configTimeout);
+/*
+ * Repeatedly pulse from low to high and back again until times runs out.
+ */
+var direction = 1;
+var data = 0;
+
+var pulse = setInterval(function() {
+	console.log('bleep');
+        rpio.pwmSetData(pin, data);
+        if (data === 0) {
+                direction = 1;
+                if (times-- === 0) {
+                        clearInterval(pulse);
+                        rpio.open(pin, rpio.INPUT);
+                        return;
+                }
+        } else if (data === max) {
+                direction = -1;
+        }
+        data += direction;
+}, interval, data, direction, times);
 
 const app = express()
 
